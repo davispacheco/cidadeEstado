45f3c0699ce4c782c2415fcfdf9a570c6c483b3f
davispacheco <davipacheco1996@gmail.com>
2022-09-02 15:30:31 -0300

    Subir parte essencial do projeto.

---
+++ src/main/java/com/lista14/cidadeEstado/CidadeEstadoApplication.java
@@ -0,0 +1,13 @@
+package com.lista14.cidadeestado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeestadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeestadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lista14/cidadeEstado/CidadeEstadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lista14.cidadeestado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeestadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
